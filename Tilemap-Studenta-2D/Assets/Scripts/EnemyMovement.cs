1783431b0799877ba0affdd3fec0f213e55af04d
Wiktoria Rekić <wiktoriarekic@gmail.com>
2022-06-13 00:38:42 +0200

    AI bug fixes

--- Tilemap-Studenta-2D/Assets/Scripts/EnemyMovement.cs
+++ Tilemap-Studenta-2D/Assets/Scripts/EnemyMovement.cs
@@ -36,11 +36,20 @@ public class EnemyMovement : MonoBehaviour
 
     private void Update()
     {
-        // Try to move in the next direction while it's queued to make movements
-        // more responsive
         if (nextDirection != Vector2.zero)
         {
+
             SetDirection(nextDirection);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Debug.Log("aaaaaaaaaaaaaaaaa");
+            this.direction = -direction;
+            nextDirection = Vector2.zero;
         }
     }
 
@@ -70,9 +79,10 @@ public class EnemyMovement : MonoBehaviour
 
     public bool Occupied(Vector2 direction)
     {
-        // If no collider is hit then there is no obstacle in that direction //0.1875f    0.04f
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, 0.15f, direction, 1f, obstacleLayer);
-        Debug.DrawRay(transform.position, direction*5, Color.magenta, 3f, false);
+        // If no collider is hit then there is no obstacle in that direction //0.1875f
+        float distance = 0.8f;
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, 0.14f, direction, distance, obstacleLayer);
+        Debug.DrawRay(transform.position, direction*distance, Color.magenta, 3f, false);
         return hit.collider != null;
     }
 
